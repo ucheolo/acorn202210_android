2c2e9fb923f08ef6f2e121b801bedcad2a768434
shinwoocheol <swc9704@gmail.com>
2023-01-13 10:43:17 +0900

    kotlin test 추가

---
+++ kotlin_test/src/main/java/com/example/kotlin_test/MyClass.kt
@@ -0,0 +1,5 @@
+package com.example.kotlin_test
+
+fun main(){
+    println("Hello, World!!")
+}
---
+++ kotlin_test/src/main/java/com/example/kotlin_test/Step02_Class.kt
@@ -0,0 +1,72 @@
+package com.example.kotlin_test
+
+// 클래스 정의하기
+class MyCar
+
+class YourCar{
+    // 멤버 함수
+    fun drive(){
+        println("달려요!")
+    }
+}
+
+class AirPlane constructor(){
+    // 생성자
+    init {
+        println("AirPlane 클래스의 init!")
+    }
+}
+// constructor() 예약어 생략가능
+class AirPlane2(){
+    // 생성자
+    init {
+        println("AirPlane2 클래스의 init!")
+    }
+}
+// 인자로 전달 받을게 없으면 () 생략가능
+class AirPlane3{
+    // 생성자
+    init {
+        println("AirPlane3 클래스의 init!")
+    }
+}
+
+/*
+    java 에서 아래와 같은 모양의 클래스를 kotlin 에서 모방하면...
+
+    public class Person{
+        public String name;
+        public Person(String name){
+            this.name=name;
+        }
+    }
+ */
+class Person constructor(name:String){ // 클래스명 옆에 선언하는 생성자를 primary 생성자라고 한다.
+    // 필드 선언
+    var name:String
+
+    init {
+        this.name=name
+    }
+}
+
+// 위의 클래스를 조금 간단히 선언하면 아래와 같다.
+class Person2(var name: String)
+// var or val 을 생성자의 인자에 선언하면 전달받은 값이 자동으로 같은 이름의 필드가 만들어지고 값이 들어간다.
+
+fun main() {
+    // 클래스를 이용해서 객체 생성 in java => new MyCar()
+    var c1 = MyCar()
+    var c2 = YourCar()
+    c2.drive()
+
+    AirPlane()
+    AirPlane2()
+    AirPlane3()
+
+    val p1 = Person("김구라")
+    println(p1.name)
+
+    val p2 = Person2("해골")
+    println(p2.name)
+}
